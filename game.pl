8c49f20531957377794ba7809153a229658ccc47
Arthurlima544 <arthurlima544@gmail.com>
2024-12-01 21:24:59 -0300

    adicionando regra para mostrar pessoas apartir de um lugar

--- game.pl
+++ game.pl
@@ -20,3 +20,11 @@ esfaqueou(Mafioso, Cidadao, Lugar) :-
     format('O cidadão ~w foi esfaqueado pelo mafioso ~w no lugar ~w.~n', [Cidadao, Mafioso, Lugar]).
 
 
+mostrar_pessoas_por_lugar(Lugar) :-
+    findall(Cidadao, (lugar(Cidadao, Lugar), cidadao(Cidadao)), Cidadaos),
+    findall(Mafioso, (lugar(Mafioso, Lugar), mafioso(Mafioso)), Mafiosos),
+    findall(Medico, (lugar(Medico, Lugar), medico(Medico)), Medicos),
+    format('No lugar ~w:~n', [Lugar]),
+    format('  Cidadãos: ~w~n', [Cidadaos]),
+    format('  Mafiosos: ~w~n', [Mafiosos]),
+    format('  Medicos: ~w~n', [Medicos]).
