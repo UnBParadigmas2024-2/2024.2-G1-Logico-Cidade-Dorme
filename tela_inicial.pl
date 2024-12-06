9ceaa34a90aed97fecd972e97fc4875dcc7a0711
Arthurlima544 <arthurlindaolima544@gmail.com>
2024-12-06 17:10:21 -0300

    Adicionando funcao iniciar jogo e alterando setup para os arquivos seguintes

---
+++ tela_inicial.pl
@@ -0,0 +1,13 @@
+/* Iniciar Jogo*/
+iniciar_jogo :-
+    write(''), nl,
+    write('=============================='), nl,
+    write('Iniciando o jogo... Todos fechem os olhos !!!'), nl,
+    write('=============================='), nl,
+    write(''), nl,
+    pressione_para_continuar,
+    mostrar_pessoas.
+    /* TODO: continuar apartir daqui */
+
+
+
