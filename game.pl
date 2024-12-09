345d74dc2f1557eaaa3b795ccab3ea8ceec31156
Arthurlima544 <arthurlindaolima544@gmail.com>
2024-12-09 12:28:57 +0000

    removendo funcoes inuteis

--- game.pl
+++ game.pl
@@ -1,6 +1,4 @@
-:- dynamic esfaqueado/2.
 /* Fatos */
-
 periodo(noite).
 
 /* Regras */
@@ -8,23 +6,13 @@ e_noite :-
     periodo(noite),
     nl.
 
-/* Se o mafioso e cidadao estao no mesmo lugar anoite, ele pode esfaquear */
-esfaqueou(Mafioso, Cidadao, Lugar) :- 
-    cidadao(Cidadao),
-    mafioso(Mafioso),
-    lugar(Mafioso, Lugar),
-    lugar(Cidadao, Lugar),
-    e_noite,
-    retract(cidadao(Cidadao)),
-    retract(lugar(Cidadao, Lugar)),
-    format('O cidadão ~w foi esfaqueado pelo mafioso ~w no lugar ~w.~n', [Cidadao, Mafioso, Lugar]).
-
+/* Mostra funcoes de cada pessoas */
 mostrar_pessoas() :-
     findall(Cidadao, cidadao(Cidadao), Cidadaos),
-    findall(Mafioso, mafioso(Mafioso), Mafiosos),
+    findall(Assassino, assassino(Assassino), Assassinos),
     findall(Anjo, anjo(Anjo), Anjos),
     findall(Detetive, detetive(Detetive), Detetives),
     format('  Cidadãos: ~w~n', [Cidadaos]),
-    format('  Mafiosos: ~w~n', [Mafiosos]),
+    format('  Assassinos: ~w~n', [Assassinos]),
     format('  Anjos: ~w~n', [Anjos]),
     format('  Detetives: ~w~n', [Detetives]).
