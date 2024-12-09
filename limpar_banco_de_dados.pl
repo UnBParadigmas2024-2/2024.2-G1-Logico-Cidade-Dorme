b321f35352e518fda2ac11bf245274e2978e9dbc
Arthurlima544 <arthurlindaolima544@gmail.com>
2024-12-09 12:24:52 +0000

    movendo arquivos e organizando em pastas

--- limpar_banco_de_dados.pl
+++
@@ -1,6 +0,0 @@
-/* Limpa as regras definidas em tempo de execucao */
-limpar_banco_de_dados :-
-    retractall(cidadao(_)),
-    retractall(mafioso(_)),
-    retractall(medico(_)),
-    retractall(lugar(_, _)).
